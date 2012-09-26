e9497816a82d1530026516032c39c244c8777f82
Carel Du Toit <careld@voxtelecom.co.za>
2012-09-26 12:11:08 +0200

    adding test files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManChild::Application.config.secret_token = '8caa759a09bedb644c576932751ddd8e53774ca424238f86c19d11bc9284107cc980f170d047a01cc5aa2f9920efaed09f602d7fdaa3ce6d2af234a252f0e890'
